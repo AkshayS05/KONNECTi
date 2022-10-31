a87e6eb4238d8ba3035022d8864bfc72b3a52f92
akshay <akshaysharma5432@gmail.com>
2022-10-31 15:44:41 +0530

    convert the code to async code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,23 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-// in order to get the name of the one of posted, we will use mongoose populate
-module.exports.home = function (req, res) {
-  // console.log(req.cookies);
-  // res.cookie('user_id', 22);
-  // Post.find({}, function (err, posts) {
-  //   return res.render('home', { title: 'KONNECTi | Home', posts: posts });
-  // });
-  Post.find({})
-    .populate('user')
-    .populate({
-      path: 'comments',
-      populate: {
-        path: 'user',
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render('home', {
-          title: 'KONNECTi | Home',
-          posts: posts,
-          all_users: users,
-        });
+module.exports.home = async function (req, res) {
+  try {
+    // in order to get the name of the one of posted, we will use mongoose populate
+    let posts = await Post.find({})
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: {
+          path: 'user',
+        },
       });
+    let users = await User.find({});
+    return res.render('home', {
+      title: 'KONNECTi | Home',
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log('Error', err);
+  }
 };
