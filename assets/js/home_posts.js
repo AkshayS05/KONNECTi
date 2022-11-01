b7cb91b2c3e9138c4729520e996f05171d4183ef
akshay <akshaysharma5432@gmail.com>
2022-11-02 04:12:57 +0530

    display new posts using Ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,7 +10,8 @@
         // the serialize will convert the data to the json
         data: newPostForm.serialize(),
         success: function (data) {
-          console.log(data);
+          let newPost = newPostDom(data.data.post);
+          $('#posts-list-container>ul').prepend(newPost);
         },
         error: function (err) {
           console.log(err.responseText);
@@ -19,5 +20,44 @@
     });
   };
   //   method to create a post in DOM
+  let newPostDom = function (post) {
+    return $(`<link rel="stylesheet" href="/css/post.css" />
+
+    <li class="single-post" id="post-${post._id}">
+      <div class="main-post">
+        <p class="main-post-content">
+       
+        ${post.content}
+          <br />
+          <small class="main-post-username"> ${post.user.name} </small>
+          </p>
+          <small>
+            <a class="delete-post-btn" href="/posts/destroy/${post.id}">X</a>
+          </small>
+      </div>
+      <div class="post-comments">
+       
+        <form action="/comments/create" method="POST" class="under-post-comment">
+          <input
+            type="text"
+            name="content"
+            placeholder="Type Here to add comment..."
+            required
+          />
+          <input type="hidden" name="post" value="${post._id}" />
+          <input type="submit" value="Add Comment" />
+        </form>
+    
+      
+    
+        <div class="post-comments-list">
+          <ul id="post-comments-${post._id}">
+           
+          </ul>
+        </div>
+      </div>
+    </li>
+    `);
+  };
   createPost();
 }
